42f1f3eee07d8637802e91414689f77346134300
Sava Savov <savamiroslavovsavov@gmail.com>
2021-06-14 15:24:55 +0300

    edit routes

--- 06 JSBackEnd/06. workshop mongoose/cubicle-part2-master/cubicle-part2-master/config/routes.js
+++ 06 JSBackEnd/06. workshop mongoose/cubicle-part2-master/cubicle-part2-master/config/routes.js
@@ -1,24 +1,14 @@
-const { about } = require("../controllers/about");
-const { attach, attachPost } = require("../controllers/details");
 const { post: commentPost } = require("../controllers/comments");
-const { createAccessory, accessoryPost } = require("../controllers/accessory");
 
 const productController = require("../controllers/productController");
-
-const { notFound } = require("../controllers/notFound");
+const accessoryController = require("../controllers/accessoryController");
+const homeController = require("../controllers/homeController");
 
 module.exports = (app) => {
-  app.get("/", (req, res) => res.redirect("/products"));
-  app.get("/about", about);
-
   app.use("/products", productController);
+  app.use("accessory", accessoryController);
 
   app.post("/comments/:cubeId/create", commentPost);
 
-  app.get("/accessory/create", createAccessory);
-  app.post("/accessory/create", accessoryPost);
-  app.get("/details/:id/attach", attach);
-  app.post("/details/:cubeId/attach", attachPost);
-
-  app.all("*", notFound);
+  app.use("/", homeController);
 };
