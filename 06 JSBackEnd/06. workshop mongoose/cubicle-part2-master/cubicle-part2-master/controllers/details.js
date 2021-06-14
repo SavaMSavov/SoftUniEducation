ec379847367d557268750b961a27f0f8158b664f
Sava Savov <savamiroslavovsavov@gmail.com>
2021-06-14 10:59:20 +0300

    edit routes

--- 06 JSBackEnd/06. workshop mongoose/cubicle-part2-master/cubicle-part2-master/controllers/details.js
+++ 06 JSBackEnd/06. workshop mongoose/cubicle-part2-master/cubicle-part2-master/controllers/details.js
@@ -1,18 +1,5 @@
 module.exports = {
-    details: async (req, res) => {
-        const cube = await req.storage.getById(req.params.id);
-
-        if (cube == undefined) {
-            res.redirect('/404');
-        } else {
-            console.log(cube);
-            const ctx = {
-                title: 'Cubicle',
-                cube
-            };
-            res.render('details', ctx);
-        }
-    },
+ 
     async attach(req, res) {
         const cube = await req.storage.getById(req.params.id);
         const accessories = await req.storage.getAllAccessories((cube.accessories || []).map(a => a._id));
