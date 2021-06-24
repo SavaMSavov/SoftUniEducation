045c7ba2e514bc690fd9210998f3f947bef1fa90
Sava Savov <savamiroslavovsavov@gmail.com>
2021-06-24 23:58:03 +0300

    add detail edit book actions

--- 06 JSBackEnd/10. examPrep/skeleton/services/hotel.js
+++ 06 JSBackEnd/10. examPrep/skeleton/services/hotel.js
@@ -1,7 +1,7 @@
 const Hotel = require("../models/Hotel");
+const User = require("../models/User");
 
 async function createHotel(hotelData) {
-
   const hotel = new Hotel(hotelData);
   await hotel.save();
 
@@ -9,18 +9,45 @@ async function createHotel(hotelData) {
 }
 
 async function getAllHotels() {
-  const hotels = await Hotel.find({});
+  const hotels = await Hotel.find({}).lean();
 
   return hotels;
 }
 async function getHotelById(id) {
-  const hotel = await Hotel.findById();
+  const hotel = await Hotel.findById(id).lean();
 
-  return hotels;
+  return hotel;
+}
+
+async function editHotel(id, hotelData) {
+  const hotel = await Hotel.findById(id);
+
+  hotel.name = hotelData.name;
+  hotel.city = hotelData.city;
+  hotel.rooms = Number(hotelData.rooms);
+  hotel.imageUrl = hotelData.imageUrl;
+
+  return hotel.save();
+}
+
+async function bookHotel(hotelId, userId) {
+  const hotel = await Hotel.findById(hotelId);
+  const user = await User.findById(userId);
+
+  if (user._id == hotel.owner) {
+    throw new Error("Cannot book hotel your own hotel.");
+  }
+
+  user.bookedHotels.push(hotelId);
+  hotel.bookedBy.push(user);
+
+  return Promise.all([user.save(), hotel.save()]);
 }
 
 module.exports = {
   createHotel,
   getAllHotels,
   getHotelById,
+  editHotel,
+  bookHotel,
 };
